ac67d4799bd77da427418d1f63a8cb1ec97ddb78
Pete Smith <5278441+aataraxiaa@users.noreply.github.com>
2025-02-20 15:47:58 +0000

    Bump Apple Toolbox to 3.1.3 (#65)
    Task/Issue URL: https://app.asana.com/0/0/1209441254376015
    
    ### Description
    Bumping Apple Toolbox (and also using PR to test the latest Asana task
    creation changes)

--- iOS/LocalPackages/Waitlist/Package.swift
+++ iOS/LocalPackages/Waitlist/Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/duckduckgo/DesignResourcesKit", exact: "3.3.1"),
-        .package(url: "https://github.com/duckduckgo/apple-toolbox.git", exact: "3.1.2"),
+        .package(url: "https://github.com/duckduckgo/apple-toolbox.git", exact: "3.1.3"),
     ],
     targets: [
         .target(
