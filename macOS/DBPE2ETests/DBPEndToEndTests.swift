zaczh/apple-browsers

commit 8b6acb272820b7c7f3141aa4afee93202b082751

AuthV2 \ macOS auth v1 and v2 (#120)
Task/Issue URL:
https://app.asana.com/0/1205842942115003/1209170372758739/f
CC: @miasma13 @diegoreymendez 

### Description

This PR implements the use of AuthV2 in the macOS app.

**The good:**

- AuthV2 is fully integrated into the iOS app.
- The authentication still uses V1 but V2 is configured and added behind
an informal feature flag. (see `isAuthV2Enabled`)

**The bad**

- We now have some code duplication, especially in the settings UI, all
will be removed alongside AuthV1
- "bridging" protocols (see SubscriptionAuthV1toV2Bridge) used where
authv1 and authv2 need to coexist
- The code is not very clean, I prioritised risk reduction and changes
minimisation over code cleanness, all will be improved when we remove
AuthV1

### Testing Steps

All tests need to be executed with App Store build and DMG build, both
using App Store and Stripe purchase flows

1. Purchase subscription
2. Enable VPN
3. Disable VPN
4. Enable VPN
5. Remove subscription
6. Restore subscription
7. Kill app and enable/disable VPN from the system menu

In general, Subscription should work as usual, no user-facing changes
are expected.

### Note to the reviewer

During development and testing, I had a lot of issues with the VPN not
being able to restart immediately after being stopped, I worked 3 days
on that problem until @diegoreymendez tested my code on his machine and
found no issues. If the problem can be replicated during testing (see
steps 2,3,4) then we'll need to fix it, if not, then it's just my
machine misbehaving.

### Quality Considerations

Most of this code is temporary and will be removed around the 24th of
April when AuthV1 will be removed

BrowserServicesKit/Sources/Subscription/Managers/AccountManager.swift
@@ -160,6 +160,20 @@ public final class DefaultAccountManager: AccountManager {
         }
     }
 
+    public func storeAccessToken(token: String) {
+        Logger.subscription.info("[AccountManager] storeAccessToken")
+
+        do {
+            try accessTokenStorage.store(accessToken: token)
+        } catch {
+            if let error = error as? AccountKeychainAccessError {
+                delegate?.accountManagerKeychainAccessFailed(accessType: .storeAccessToken, error: error)
+            } else {
+                assertionFailure("Expected AccountKeychainAccessError")
+            }
+        }
+    }
+
     public func storeAccount(token: String, email: String?, externalID: String?) {
         Logger.subscription.info("[AccountManager] storeAccount")
 

iOS/PacketTunnelProvider/NetworkProtection/NetworkProtectionPacketTunnelProvider.swift
@@ -37,8 +37,7 @@ final class NetworkProtectionPacketTunnelProvider: PacketTunnelProvider {
     private static var vpnLogger = VPNLogger()
     private static let persistentPixel: PersistentPixelFiring = PersistentPixel()
     private var cancellables = Set<AnyCancellable>()
-
-    static private let isAuthV2Enabled = false
+    
     private var accountManager: AccountManager?
     private let subscriptionManager: (any SubscriptionManagerV2)?
 
@@ -436,7 +435,7 @@ final class NetworkProtectionPacketTunnelProvider: PacketTunnelProvider {
             subscriptionEnvironment.serviceEnvironment = .staging
         }
 
-        // MARK: - Configure Subscription ------------------------------------------------------------------------------------------------------------
+        // MARK: - Configure Subscription
 
         var tokenHandler: any SubscriptionTokenHandling
         var entitlementsCheck: (() async -> Result<Bool, Error>)
@@ -524,8 +523,9 @@ final class NetworkProtectionPacketTunnelProvider: PacketTunnelProvider {
                                                                    subscriptionEnvironment: subscriptionEnvironment,
                                                                    pixelHandler: pixelHandler,
                                                                    autoRecoveryHandler: {
-                // TODO Implement
-            })
+                // todo Implement
+            },
+                                                                   initForPurchase: false)
             self.subscriptionManager = subscriptionManager
             
             entitlementsCheck = {
@@ -538,7 +538,7 @@ final class NetworkProtectionPacketTunnelProvider: PacketTunnelProvider {
             self.accountManager = nil
         }
 
-        // MARK: - -----------------------------------------------------------------------------------------------------------------------------------
+        // MARK: -
 
         let errorStore = NetworkProtectionTunnelErrorStore()
         let notificationsPresenter = NetworkProtectionUNNotificationPresenter()

commit 4bb5c633f0867b094b235d0dcf080c2901d1ef36

VPN and PIR issues fix (#218)
Task/Issue URL:
https://app.asana.com/0/1205842942115003/1209654847406840/f
CC: @diegoreymendez  @THISISDINOSAUR @samsymons @miasma13 

### Description

Fix for the token issue introduced in
https://github.com/duckduckgo/apple-browsers/pull/120

What happened:

**VPN:**

Different parts of the app use different ways to store and retrieve the
access token.
The `DefaultAccountManager` is used in the main app but not in the
SysExt where `NetworkProtectionKeychainTokenStore` is used directly. The
two classes use different keys to store the access token.
In [a recent PR](https://github.com/duckduckgo/apple-browsers/pull/120),
I mistakenly swapped the classes that were initialised side by side and
implemented the same protocol (`SubscriptionTokenHandling`), so no error
was detected at build time.

Fixes:
- In `MacPacketTunnelProvider` I restored the `tokenHandler =
tokenStore`

**PIR**
The issue with PIR was only with the e2e tests, the way the token is
overwritten during the tests didn't result in the SubscriptionManager
thinking that the user was logged in.

Memory improvements by @samsymons see details here:
https://app.asana.com/0/1193060753475688/1209661386167901

### Testing Steps
1. Subscribe to privacy pro
2. Start VPN
3. Start DBP

---
###### Internal references:
[Definition of
Done](https://app.asana.com/0/1202500774821704/1207634633537039/f) |
[Engineering
Expectations](https://app.asana.com/0/59792373528535/199064865822552) |
[Tech Design
Template](https://app.asana.com/0/59792373528535/184709971311943)

---------

Co-authored-by: Sam Symons <sam@samsymons.com>

BrowserServicesKit/Sources/Subscription/Managers/AccountManager.swift
@@ -21,13 +21,15 @@ import Common
 import os.log
 
 public protocol AccountManagerKeychainAccessDelegate: AnyObject {
-    func accountManagerKeychainAccessFailed(accessType: AccountKeychainAccessType, error: AccountKeychainAccessError)
+    func accountManagerKeychainAccessFailed(accessType: AccountKeychainAccessType, error: any Error)
 }
 
 public protocol AccountManager {
 
     var delegate: AccountManagerKeychainAccessDelegate? { get set }
+    /// The `accessToken` is long lasting and is used to authenticate API requests and VPN connections
     var accessToken: String? { get }
+    /// The `authToken` is short lasting and is obtained when the user purchases the subscription, is immediately exchanged for a long lasting `accessToken`
     var authToken: String? { get }
     var email: String? { get }
     var externalID: String? { get }
@@ -94,12 +96,7 @@ public final class DefaultAccountManager: AccountManager {
         do {
             return try storage.getAuthToken()
         } catch {
-            if let error = error as? AccountKeychainAccessError {
-                delegate?.accountManagerKeychainAccessFailed(accessType: .getAuthToken, error: error)
-            } else {
-                assertionFailure("Expected AccountKeychainAccessError")
-            }
-
+            delegate?.accountManagerKeychainAccessFailed(accessType: .getAuthToken, error: error)
             return nil
         }
     }
@@ -108,12 +105,7 @@ public final class DefaultAccountManager: AccountManager {
         do {
             return try accessTokenStorage.getAccessToken()
         } catch {
-            if let error = error as? AccountKeychainAccessError {
-                delegate?.accountManagerKeychainAccessFailed(accessType: .getAccessToken, error: error)
-            } else {
-                assertionFailure("Expected AccountKeychainAccessError")
-            }
-
+            delegate?.accountManagerKeychainAccessFailed(accessType: .getAccessToken, error: error)
             return nil
         }
     }
@@ -122,12 +114,7 @@ public final class DefaultAccountManager: AccountManager {
         do {
             return try storage.getEmail()
         } catch {
-            if let error = error as? AccountKeychainAccessError {
-                delegate?.accountManagerKeychainAccessFailed(accessType: .getEmail, error: error)
-            } else {
-                assertionFailure("Expected AccountKeychainAccessError")
-            }
-
+            delegate?.accountManagerKeychainAccessFailed(accessType: .getEmail, error: error)
             return nil
         }
     }
@@ -136,12 +123,7 @@ public final class DefaultAccountManager: AccountManager {
         do {
             return try storage.getExternalID()
         } catch {
-            if let error = error as? AccountKeychainAccessError {
-                delegate?.accountManagerKeychainAccessFailed(accessType: .getExternalID, error: error)
-            } else {
-                assertionFailure("Expected AccountKeychainAccessError")
-            }
-
+            delegate?.accountManagerKeychainAccessFailed(accessType: .getExternalID, error: error)
             return nil
         }
     }
@@ -152,11 +134,7 @@ public final class DefaultAccountManager: AccountManager {
         do {
             try storage.store(authToken: token)
         } catch {
-            if let error = error as? AccountKeychainAccessError {
-                delegate?.accountManagerKeychainAccessFailed(accessType: .storeAuthToken, error: error)
-            } else {
-                assertionFailure("Expected AccountKeychainAccessError")
-            }
+            delegate?.accountManagerKeychainAccessFailed(accessType: .storeAuthToken, error: error)
         }
     }
 
@@ -166,11 +144,7 @@ public final class DefaultAccountManager: AccountManager {
         do {
             try accessTokenStorage.store(accessToken: token)
         } catch {
-            if let error = error as? AccountKeychainAccessError {
-                delegate?.accountManagerKeychainAccessFailed(accessType: .storeAccessToken, error: error)
-            } else {
-                assertionFailure("Expected AccountKeychainAccessError")
-            }
+            delegate?.accountManagerKeychainAccessFailed(accessType: .storeAccessToken, error: error)
         }
     }
 
@@ -180,31 +154,19 @@ public final class DefaultAccountManager: AccountManager {
         do {
             try accessTokenStorage.store(accessToken: token)
         } catch {
-            if let error = error as? AccountKeychainAccessError {
-                delegate?.accountManagerKeychainAccessFailed(accessType: .storeAccessToken, error: error)
-            } else {
-                assertionFailure("Expected AccountKeychainAccessError")
-            }
+            delegate?.accountManagerKeychainAccessFailed(accessType: .storeAccessToken, error: error)
         }
 
         do {
             try storage.store(email: email)
         } catch {
-            if let error = error as? AccountKeychainAccessError {
-                delegate?.accountManagerKeychainAccessFailed(accessType: .storeEmail, error: error)
-            } else {
-                assertionFailure("Expected AccountKeychainAccessError")
-            }
+            delegate?.accountManagerKeychainAccessFailed(accessType: .storeEmail, error: error)
         }
 
         do {
             try storage.store(externalID: externalID)
         } catch {
-            if let error = error as? AccountKeychainAccessError {
-                delegate?.accountManagerKeychainAccessFailed(accessType: .storeExternalID, error: error)
-            } else {
-                assertionFailure("Expected AccountKeychainAccessError")
-            }
+            delegate?.accountManagerKeychainAccessFailed(accessType: .storeExternalID, error: error)
         }
         NotificationCenter.default.post(name: .accountDidSignIn, object: self, userInfo: nil)
     }
@@ -222,11 +184,7 @@ public final class DefaultAccountManager: AccountManager {
             subscriptionEndpointService.signOut()
             entitlementsCache.reset()
         } catch {
-            if let error = error as? AccountKeychainAccessError {
-                delegate?.accountManagerKeychainAccessFailed(accessType: .clearAuthenticationData, error: error)
-            } else {
-                assertionFailure("Expected AccountKeychainAccessError")
-            }
+            delegate?.accountManagerKeychainAccessFailed(accessType: .clearAuthenticationData, error: error)
         }
 
         if !skipNotification {

BrowserServicesKit/Sources/Subscription/Storage/V1/AccountKeychainStorage.swift
@@ -30,7 +30,7 @@ public enum AccountKeychainAccessType: String {
     case clearAuthenticationData
 }
 
-public enum AccountKeychainAccessError: Error, Equatable {
+public enum AccountKeychainAccessError: Error, LocalizedError {
     case failedToDecodeKeychainData
     case failedToDecodeKeychainValueAsData
     case failedToDecodeKeychainDataAsString

BrowserServicesKit/Sources/SubscriptionTestingUtilities/AccountStorage/AccountManagerKeychainAccessDelegateMock.swift
@@ -21,13 +21,13 @@ import Subscription
 
 public final class AccountManagerKeychainAccessDelegateMock: AccountManagerKeychainAccessDelegate {
 
-    public var onAccountManagerKeychainAccessFailed: ((AccountKeychainAccessType, AccountKeychainAccessError) -> Void)?
+    public var onAccountManagerKeychainAccessFailed: ((AccountKeychainAccessType, any Error) -> Void)?
 
-    public init(onAccountManagerKeychainAccessFailed: ( (AccountKeychainAccessType, AccountKeychainAccessError) -> Void)? = nil) {
+    public init(onAccountManagerKeychainAccessFailed: ( (AccountKeychainAccessType, any Error) -> Void)? = nil) {
         self.onAccountManagerKeychainAccessFailed = onAccountManagerKeychainAccessFailed
     }
 
-    public func accountManagerKeychainAccessFailed(accessType: AccountKeychainAccessType, error: AccountKeychainAccessError) {
+    public func accountManagerKeychainAccessFailed(accessType: AccountKeychainAccessType, error: any Error) {
         onAccountManagerKeychainAccessFailed?(accessType, error)
     }
 }

iOS/DuckDuckGo/Subscription/DefaultSubscriptionManager+AccountManagerKeychainAccessDelegate.swift
@@ -23,10 +23,10 @@ import Subscription
 
 extension DefaultSubscriptionManager: AccountManagerKeychainAccessDelegate {
 
-    public func accountManagerKeychainAccessFailed(accessType: AccountKeychainAccessType, error: AccountKeychainAccessError) {
+    public func accountManagerKeychainAccessFailed(accessType: AccountKeychainAccessType, error: any Error) {
         let parameters = [
             PixelParameters.privacyProKeychainAccessType: accessType.rawValue,
-            PixelParameters.privacyProKeychainError: error.errorDescription,
+            PixelParameters.privacyProKeychainError: error.localizedDescription,
             PixelParameters.source: "browser"
         ]
 

iOS/PacketTunnelProvider/NetworkProtection/NetworkProtectionPacketTunnelProvider.swift
@@ -670,10 +670,10 @@ final class DefaultWireGuardInterface: WireGuardInterface {
 
 extension NetworkProtectionPacketTunnelProvider: AccountManagerKeychainAccessDelegate {
 
-    public func accountManagerKeychainAccessFailed(accessType: AccountKeychainAccessType, error: AccountKeychainAccessError) {
+    public func accountManagerKeychainAccessFailed(accessType: AccountKeychainAccessType, error: any Error) {
         let parameters = [
             PixelParameters.privacyProKeychainAccessType: accessType.rawValue,
-            PixelParameters.privacyProKeychainError: error.errorDescription,
+            PixelParameters.privacyProKeychainError: error.localizedDescription,
             PixelParameters.source: "vpn"
         ]
 

macOS/DuckDuckGo/NetworkProtection/NetworkExtensionTargets/NetworkExtensionTargets/MacPacketTunnelProvider.swift
@@ -458,7 +458,7 @@ final class MacPacketTunnelProvider: PacketTunnelProvider {
             }
 
             self.accountManager = accountManager
-            tokenHandler = accountManager
+            tokenHandler = tokenStore
         } else {
             // MARK: V2
             let configuration = URLSessionConfiguration.default
@@ -769,7 +769,7 @@ final class DefaultWireGuardInterface: WireGuardInterface {
 
 extension MacPacketTunnelProvider: AccountManagerKeychainAccessDelegate {
 
-    public func accountManagerKeychainAccessFailed(accessType: AccountKeychainAccessType, error: AccountKeychainAccessError) {
+    public func accountManagerKeychainAccessFailed(accessType: AccountKeychainAccessType, error: any Error) {
         PixelKit.fire(PrivacyProErrorPixel.privacyProKeychainAccessError(accessType: accessType, accessError: error),
                       frequency: .legacyDailyAndCount)
     }

macOS/DuckDuckGo/Statistics/PrivacyProPixel.swift
@@ -133,7 +133,7 @@ enum PrivacyProPixel: PixelKitEventV2 {
 
 enum PrivacyProErrorPixel: PixelKitEventV2 {
 
-    case privacyProKeychainAccessError(accessType: AccountKeychainAccessType, accessError: AccountKeychainAccessError)
+    case privacyProKeychainAccessError(accessType: AccountKeychainAccessType, accessError: any Error)
 
     var name: String {
         switch self {
@@ -146,7 +146,7 @@ enum PrivacyProErrorPixel: PixelKitEventV2 {
         case .privacyProKeychainAccessError(let accessType, let accessError):
             return [
                 "type": accessType.rawValue,
-                "error": accessError.errorDescription
+                "error": accessError.localizedDescription
             ]
         }
     }

macOS/DuckDuckGo/Subscription/SubscriptionManager+StandardConfiguration.swift
@@ -92,7 +92,7 @@ extension DefaultSubscriptionManager {
 
 extension DefaultSubscriptionManager: AccountManagerKeychainAccessDelegate {
 
-    public func accountManagerKeychainAccessFailed(accessType: AccountKeychainAccessType, error: AccountKeychainAccessError) {
+    public func accountManagerKeychainAccessFailed(accessType: AccountKeychainAccessType, error: any Error) {
         PixelKit.fire(PrivacyProErrorPixel.privacyProKeychainAccessError(accessType: accessType, accessError: error),
                       frequency: .legacyDailyAndCount)
     }

macOS/DuckDuckGoVPN/DuckDuckGoVPNAppDelegate.swift
@@ -571,7 +571,7 @@ final class DuckDuckGoVPNAppDelegate: NSObject, NSApplicationDelegate {
 
 extension DuckDuckGoVPNAppDelegate: AccountManagerKeychainAccessDelegate {
 
-    public func accountManagerKeychainAccessFailed(accessType: AccountKeychainAccessType, error: AccountKeychainAccessError) {
+    public func accountManagerKeychainAccessFailed(accessType: AccountKeychainAccessType, error: any Error) {
         PixelKit.fire(PrivacyProErrorPixel.privacyProKeychainAccessError(accessType: accessType, accessError: error),
                       frequency: .legacyDailyAndCount)
     }

macOS/LocalPackages/DataBrokerProtection/Sources/DataBrokerProtection/Authentication/DataBrokerProtectionAuthenticationManaging.swift
@@ -36,7 +36,15 @@ public final class DataBrokerProtectionAuthenticationManager: DataBrokerProtecti
     private let subscriptionManager: DataBrokerProtectionSubscriptionManaging
 
     public var isUserAuthenticated: Bool {
-        subscriptionManager.isUserAuthenticated
+        var token: String?
+        // extremely ugly hack, will be removed as soon auth v1 is removed
+        let semaphore = DispatchSemaphore(value: 0)
+        Task {
+            token = await accessToken()
+            semaphore.signal()
+        }
+        semaphore.wait()
+        return token != nil
     }
 
     public func accessToken() async -> String? {

macOS/LocalPackages/DataBrokerProtection/Sources/DataBrokerProtection/Authentication/DataBrokerProtectionSubscriptionManaging.swift
@@ -22,7 +22,6 @@ import Common
 import AppKitExtensions
 
 public protocol DataBrokerProtectionSubscriptionManaging {
-    var isUserAuthenticated: Bool { get }
     func accessToken() async -> String?
     func hasValidEntitlement() async throws -> Bool
 }
@@ -30,10 +29,6 @@ public protocol DataBrokerProtectionSubscriptionManaging {
 public final class DataBrokerProtectionSubscriptionManager: DataBrokerProtectionSubscriptionManaging {
 
     let subscriptionManager: any SubscriptionAuthV1toV2Bridge
-
-    public var isUserAuthenticated: Bool {
-        subscriptionManager.isUserAuthenticated
-    }
 
     public func accessToken() async -> String? {
         // We use a staging token for privacy pro supplied through a github secret/action

BrowserServicesKit/Tests/SubscriptionTests/Managers/AccountManagerTests.swift
@@ -95,7 +95,7 @@ final class AccountManagerTests: XCTestCase {
         let keychainAccessDelegateMock = AccountManagerKeychainAccessDelegateMock { type, error in
             delegateCalled.fulfill()
             XCTAssertEqual(type, .storeAuthToken)
-            XCTAssertEqual(error, Constants.keychainError)
+            XCTAssertEqual(error.localizedDescription, Constants.keychainError.localizedDescription)
         }
 
         accountStorage.mockedAccessError = Constants.keychainError

macOS/DBPE2ETests/DBPEndToEndTests.swift
@@ -125,12 +125,16 @@ final class DBPEndToEndTests: XCTestCase {
         await awaitFulfillment(of: profileSavedExpectation,
                                withTimeout: 3,
                                whenCondition: {
-            try! database.fetchProfile() != nil
+            autoreleasepool { // All autoreleasepool uses have been added as part of https://app.asana.com/0/1193060753475688/1209661386167901 in order to bring down the memory usage from 20Gb+ to 60-70Mb
+                try! database.fetchProfile() != nil
+            }
         })
         await awaitFulfillment(of: profileQueriesCreatedExpectation,
                                withTimeout: 3,
                                whenCondition: {
-            try! database.fetchAllBrokerProfileQueryData().count > 0
+            autoreleasepool {
+                try! database.fetchAllBrokerProfileQueryData().count > 0
+            }
         })
 
         // Also check that we made the broker profile queries correctly
@@ -183,10 +187,12 @@ final class DBPEndToEndTests: XCTestCase {
         await awaitFulfillment(of: extractedProfilesFoundExpectation,
                                withTimeout: 60,
                                whenCondition: {
-            let queries = try! database.fetchAllBrokerProfileQueryData()
-            let brokerIDs = queries.compactMap { $0.dataBroker.id }
-            let extractedProfiles = brokerIDs.flatMap { try! database.fetchExtractedProfiles(for: $0) }
-            return extractedProfiles.count > 0
+            autoreleasepool {
+                let queries = try! database.fetchAllBrokerProfileQueryData()
+                let brokerIDs = queries.compactMap { $0.dataBroker.id }
+                let extractedProfiles = brokerIDs.flatMap { try! database.fetchExtractedProfiles(for: $0) }
+                return extractedProfiles.count > 0
+            }
         })
 
         print("Stage 3 passed: We find and save extracted profiles")
@@ -199,9 +205,11 @@ final class DBPEndToEndTests: XCTestCase {
         await awaitFulfillment(of: optOutJobsCreatedExpectation,
                                withTimeout: 10,
                                whenCondition: {
-            let queries = try! database.fetchAllBrokerProfileQueryData()
-            let optOutJobs = queries.flatMap { $0.optOutJobData }
-            return optOutJobs.count > 0
+            autoreleasepool {
+                let queries = try! database.fetchAllBrokerProfileQueryData()
+                let optOutJobs = queries.flatMap { $0.optOutJobData }
+                return optOutJobs.count > 0
+            }
         })
 
         print("Stage 4 passed: We create opt out jobs")
@@ -215,9 +223,11 @@ final class DBPEndToEndTests: XCTestCase {
         await awaitFulfillment(of: optOutJobsRunExpectation,
                                withTimeout: 300,
                                whenCondition: {
-            let queries = try! database.fetchAllBrokerProfileQueryData()
-            let optOutJobs = queries.flatMap { $0.optOutJobData }
-            return optOutJobs.first?.lastRunDate != nil
+            autoreleasepool {
+                let queries = try! database.fetchAllBrokerProfileQueryData()
+                let optOutJobs = queries.flatMap { $0.optOutJobData }
+                return optOutJobs.first?.lastRunDate != nil
+            }
         })
         print("Stage 5.1 passed: We start running the opt out jobs")
 
@@ -279,11 +289,13 @@ final class DBPEndToEndTests: XCTestCase {
         await awaitFulfillment(of: optOutConfirmedExpectation,
                                withTimeout: 600,
                                whenCondition: {
-            let queries = try! database.fetchAllBrokerProfileQueryData()
-            let optOutJobs = queries.flatMap { $0.optOutJobData }
-            let events = optOutJobs.flatMap { $0.historyEvents }
-            let optOutsConfirmed = events.filter{ $0.type == .optOutConfirmed }
-            return optOutsConfirmed.count > 0
+            autoreleasepool {
+                let queries = try! database.fetchAllBrokerProfileQueryData()
+                let optOutJobs = queries.flatMap { $0.optOutJobData }
+                let events = optOutJobs.flatMap { $0.historyEvents }
+                let optOutsConfirmed = events.filter{ $0.type == .optOutConfirmed }
+                return optOutsConfirmed.count > 0
+            }
         })
         print("Stage 9 passed: We confirm the opt out through a scan")
     }

macOS/LocalPackages/DataBrokerProtection/Tests/DataBrokerProtectionTests/DataBrokerProtectionAuthenticationManagerTests.swift
@@ -33,7 +33,7 @@ class DataBrokerProtectionAuthenticationManagerTests: XCTestCase {
     }
 
     func testUserNotAuthenticatedWhenSubscriptionManagerReturnsFalse() {
-        subscriptionManager.userAuthenticatedValue = false
+        subscriptionManager.accessTokenValue = nil
 
         authenticationManager = DataBrokerProtectionAuthenticationManager(subscriptionManager: subscriptionManager)
 
@@ -50,7 +50,7 @@ class DataBrokerProtectionAuthenticationManagerTests: XCTestCase {
     }
 
     func testUserAuthenticatedWhenSubscriptionManagerReturnsTrue() {
-        subscriptionManager.userAuthenticatedValue = true
+        subscriptionManager.accessTokenValue = "some"
 
         authenticationManager = DataBrokerProtectionAuthenticationManager(subscriptionManager: subscriptionManager)
 