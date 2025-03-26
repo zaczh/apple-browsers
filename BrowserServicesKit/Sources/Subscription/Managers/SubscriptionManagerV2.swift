a29874b7332207327211322f3bed015ceaa4a145
Federico Cappelli <fcappelli@duckduckgo.com>
2025-03-26 20:42:38 +0000

    [WIP] Authv2 - isUserAuthenticated + migration fix (#348)
    Task/Issue URL: https://app.asana.com/0/0/1209726536726503
    CC: @diegoreymendez 
    
    ### Description
    
    AuthV2 improvements

--- BrowserServicesKit/Sources/Subscription/Managers/SubscriptionManagerV2.swift
+++ BrowserServicesKit/Sources/Subscription/Managers/SubscriptionManagerV2.swift
@@ -234,7 +234,6 @@ public final class DefaultSubscriptionManagerV2: SubscriptionManagerV2 {
         guard v1MigrationNeeded else {
             return
         }
-        v1MigrationNeeded = false
 
         // Attempting V1 token migration
         do {
@@ -243,6 +242,8 @@ public final class DefaultSubscriptionManagerV2: SubscriptionManagerV2 {
 
                 // cleaning up old data
                 clearSubscriptionCache()
+
+                v1MigrationNeeded = false
             }
         } catch {
             Logger.subscription.error("Failed to migrate V1 token: \(error, privacy: .public)")
@@ -271,7 +272,7 @@ public final class DefaultSubscriptionManagerV2: SubscriptionManagerV2 {
 
     @discardableResult
     public func getSubscription(cachePolicy: SubscriptionCachePolicy) async throws -> PrivacyProSubscription {
-        guard isUserAuthenticated else {
+        guard await isUserAuthenticated() else {
             throw SubscriptionEndpointServiceError.noData
         }
 
@@ -343,7 +344,7 @@ public final class DefaultSubscriptionManagerV2: SubscriptionManagerV2 {
     }
 
     public func getCustomerPortalURL() async throws -> URL {
-        guard isUserAuthenticated else {
+        guard await isUserAuthenticated() else {
             throw SubscriptionEndpointServiceError.noData
         }
 
@@ -358,7 +359,20 @@ public final class DefaultSubscriptionManagerV2: SubscriptionManagerV2 {
 
     // MARK: - User
     public var isUserAuthenticated: Bool {
-        oAuthClient.isUserAuthenticated
+        var tokenContainer: TokenContainer?
+        // extremely ugly hack, will be replaced by `func isUserAuthenticated()` as soon auth v1 is removed
+        let semaphore = DispatchSemaphore(value: 0)
+        Task {
+            tokenContainer = try? await getTokenContainer(policy: .localValid)
+            semaphore.signal()
+        }
+        semaphore.wait()
+        return tokenContainer != nil
+    }
+
+    private func isUserAuthenticated() async -> Bool {
+        let token = try? await getTokenContainer(policy: .local)
+        return token != nil
     }
 
     public var userEmail: String? {
@@ -465,7 +479,7 @@ public final class DefaultSubscriptionManagerV2: SubscriptionManagerV2 {
     /// - Parameter forceRefresh: ignore subscription and token cache and re-download everything
     /// - Returns: An Array of SubscriptionFeature where each feature is enabled or disabled based on the user entitlements
     public func currentSubscriptionFeatures(forceRefresh: Bool) async throws -> [SubscriptionFeatureV2] {
-        guard isUserAuthenticated else { return [] }
+        guard await isUserAuthenticated() else { return [] }
 
         var userEntitlements: [SubscriptionEntitlement]
         var availableFeatures: [SubscriptionEntitlement]
@@ -495,7 +509,7 @@ public final class DefaultSubscriptionManagerV2: SubscriptionManagerV2 {
     }
 
     public func isFeatureAvailableForUser(_ entitlement: SubscriptionEntitlement) async throws -> Bool {
-        guard isUserAuthenticated else { return false }
+        guard await isUserAuthenticated() else { return false }
 
         let currentFeatures = try await currentSubscriptionFeatures(forceRefresh: false)
         return currentFeatures.contains { feature in
